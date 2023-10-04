058221dfd3ce822a149839186ecf41d33cdca9be
ThiagoMiranda27 <thi_cmiranda@hotmail.com>
2023-10-04 19:53:00 -0300

    start project

---
+++ src/main/java/br/com/thiagomiranda/wefluxcourse/WefluxCourseApplication.java
@@ -0,0 +1,13 @@
+ package br.com.thiagomiranda.wefluxcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefluxCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WefluxCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/thiagomiranda/wefluxcourse/WefluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagomiranda.wefluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WefluxCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
