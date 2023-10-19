3f564ccce0a39ae9c61fab9bab42759b961e07c8
ThiagoMiranda27 <thi_cmiranda@hotmail.com>
2023-10-19 19:34:25 -0300

    add String validator

--- src/main/java/br/com/thiagomiranda/wefluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/thiagomiranda/wefluxcourse/model/request/UserRequest.java
@@ -1,17 +1,21 @@
 package br.com.thiagomiranda.wefluxcourse.model.request;
 
+import br.com.thiagomiranda.wefluxcourse.validator.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
 
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
         @NotBlank(message = "must not be null or empty")
         String name,
+        @TrimString
         @Email(message = "invalid email")
         @NotBlank(message = "must not be null or empty")
         String email,
+        @TrimString
         @Size(min = 3, max = 50, message = "must be between 3 and 20 characters")
         @NotBlank(message = "must not be null or empty")
         String password
