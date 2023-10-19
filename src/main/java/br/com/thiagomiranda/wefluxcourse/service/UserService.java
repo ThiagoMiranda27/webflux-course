9111162736b7675e81ac3534223096e8b9014c7a
ThiagoMiranda27 <thi_cmiranda@hotmail.com>
2023-10-19 20:00:52 -0300

    add exception handler to Object not found

--- src/main/java/br/com/thiagomiranda/wefluxcourse/service/UserService.java
+++ src/main/java/br/com/thiagomiranda/wefluxcourse/service/UserService.java
@@ -4,9 +4,12 @@ import br.com.thiagomiranda.wefluxcourse.entity.User;
 import br.com.thiagomiranda.wefluxcourse.mapper.UserMapper;
 import br.com.thiagomiranda.wefluxcourse.model.request.UserRequest;
 import br.com.thiagomiranda.wefluxcourse.repository.UserRepository;
+import br.com.thiagomiranda.wefluxcourse.service.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +24,12 @@ public class UserService {
     }
 
     public Mono<User> findById(final String id){
-        return repository.findById(id);
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(
+                        new ObjectNotFoundException(
+                                format("Object not found. Id: %s, Type %s", id, User.class.getSimpleName())
+                        )
+                ));
     }
 
 }
