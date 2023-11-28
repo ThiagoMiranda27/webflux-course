62969502f249dd20ad2bb80d678fab490e54fba8
ThiagoMiranda27 <thi_cmiranda@hotmail.com>
2023-11-28 18:42:18 -0300

    add unit test to delete user service

--- src/test/java/br/com/thiagomiranda/wefluxcourse/service/UserServiceTest.java
+++ src/test/java/br/com/thiagomiranda/wefluxcourse/service/UserServiceTest.java
@@ -94,4 +94,19 @@ class UserServiceTest {
         verify(repository, times(1)).save(any(User.class));
     }
 
+    @Test
+    void testDelete(){
+        User entity = User.builder().build();
+        when(repository.findAndRemove(anyString())).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.delete("123");
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).findAndRemove(anyString());
+    }
+
 }
