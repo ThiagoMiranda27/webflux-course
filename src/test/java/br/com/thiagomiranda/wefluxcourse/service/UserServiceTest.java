1fb17423fab6a8f88fe32a317efe9167f204a73f
ThiagoMiranda27 <thi_cmiranda@hotmail.com>
2023-11-28 18:38:53 -0300

    add unit test to Update user service

--- src/test/java/br/com/thiagomiranda/wefluxcourse/service/UserServiceTest.java
+++ src/test/java/br/com/thiagomiranda/wefluxcourse/service/UserServiceTest.java
@@ -6,18 +6,16 @@ import br.com.thiagomiranda.wefluxcourse.model.request.UserRequest;
 import br.com.thiagomiranda.wefluxcourse.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -36,8 +34,8 @@ class UserServiceTest {
         UserRequest request = new UserRequest("thiago", "thiago@email.com", "123");
         User entity = User.builder().build();
 
-        Mockito.when(mapper.toEntity(ArgumentMatchers.any(UserRequest.class))).thenReturn(entity);
-        Mockito.when(repository.save(ArgumentMatchers.any(User.class))).thenReturn(Mono.just(User.builder().build()));
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
 
         Mono<User> result = service.save(request);
 
@@ -46,7 +44,7 @@ class UserServiceTest {
                 .expectComplete()
                 .verify();
 
-        Mockito.verify(repository, times(1)).save(ArgumentMatchers.any(User.class));
+        verify(repository, times(1)).save(any(User.class));
     }
 
     @Test
@@ -60,7 +58,7 @@ class UserServiceTest {
                 .expectComplete()
                 .verify();
 
-        Mockito.verify(repository, times(1)).findById(ArgumentMatchers.anyString());
+        verify(repository, times(1)).findById(anyString());
     }
 
     @Test
@@ -74,7 +72,26 @@ class UserServiceTest {
                 .expectComplete()
                 .verify();
 
-        Mockito.verify(repository, times(1)).findAll();
+        verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    void testUpdate(){
+        UserRequest request = new UserRequest("thiago", "thiago@email.com", "123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class), any(User.class))).thenReturn(entity);
+        when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+        when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.update("123",request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).save(any(User.class));
     }
 
 }
